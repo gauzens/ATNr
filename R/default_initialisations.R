a7490942d09adac1e35bbf99c11365ce9262e7dd
gauzens <gauzens@gmail.com>
2022-08-03 11:27:29 +0200

    fixing wth(...,)

--- R/default_initialisations.R
+++ R/default_initialisations.R
@@ -174,7 +174,7 @@ initialise_default_Scaled <- function(model) {
   model$w <- w[, (model$nb_b + 1):model$nb_s]
 
   # per gram metabolic rate
-  min.BM = min(BM[1:nb_b])
+  min.BM =  with(schneider, min(BM[1:nb_b]))
   model$X <- with(schneider, 0.314 * BM^-0.25 / min.BM^-0.25)
   # model$X[1:schneider$nb_b] <- 0.0
   # species efficiencies
